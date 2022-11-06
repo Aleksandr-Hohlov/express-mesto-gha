200ca91f7f60281bd15d03780d11b70a8aace8da
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-06 21:09:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const { STATUS, ERROR_MESSAGE } = require('./constants/constants');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
-const { PORT = 3000 } = process.env;
-mongoose.connect('mongodb://localhost:27017/mestodb'); // подключаемся к базе данных
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+app.use(helmet());
+app.disable('x-powered-by');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(cookieParser());
-
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+// временная авторизация
 app.use((req, res, next) => {
   req.user = {
     _id: '6364012305b1c3eaf904ca3f',
@@ -22,7 +37,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/cards', cardsRouter);
-app.use('/users', usersRouter);
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
+
+app.use('*', (req, res) => { res.status(STATUS.NOT_FOUND).send({ message: ERROR_MESSAGE.NOT_FOUND.PAGE }); });
 
 app.listen(PORT);
