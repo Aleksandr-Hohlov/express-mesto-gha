57891c95ac5d666b65258dcae45f742dee8ab4ac
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-06 23:11:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
+const { STATUS, ERROR_MESSAGE } = require('./constants/constants');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+app.use(helmet());
+app.disable('x-powered-by');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+// временная авторизация
 app.use((req, res, next) => {
   req.user = {
     _id: '636812fe2b2fabadd45ea998',
   };
 
   next();
+});
+
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
+
+app.use('*', (req, res) => {
+  res.status(STATUS.NOT_FOUND).send({ message: ERROR_MESSAGE.NOT_FOUND.PAGE });
 });
 
 app.listen(PORT);
