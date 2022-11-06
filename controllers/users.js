c30a609aa33a17499b13236af4d8696cb82a7687
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-07 01:50:32 +0300

    Add files via upload

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,12 @@
 const User = require('../models/user');
 
-module.exports.getAllUsers = (req, res) => {
+const getAllUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
     .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
 };
 
-module.exports.getUserById = (req, res) => {
+const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (user) {
@@ -24,7 +24,7 @@ module.exports.getUserById = (req, res) => {
     });
 };
 
-module.exports.createUser = (req, res) => {
+const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
@@ -37,7 +37,7 @@ module.exports.createUser = (req, res) => {
     });
 };
 
-module.exports.updateUserInfo = (req, res) => {
+const updateUserInfo = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
@@ -63,7 +63,7 @@ module.exports.updateUserInfo = (req, res) => {
     });
 };
 
-module.exports.updateAvatar = (req, res) => {
+const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => {
@@ -80,4 +80,12 @@ module.exports.updateAvatar = (req, res) => {
         res.status(500).send({ message: 'Ошибка по умолчанию' });
       }
     });
+};
+
+module.exports = {
+  getUserById,
+  getAllUsers,
+  createUser,
+  updateUserInfo,
+  updateAvatar,
 };
