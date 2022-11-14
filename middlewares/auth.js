81cc089cb7f05aeab3d01121837505333cddbf3f
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-15 01:15:09 +0300

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,18 +3,21 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Сессия истекла. Повторите авторизацию.');
+
+  if (!authorization) {
+    throw new UnauthorizedError('Необходима авторизация');
   }
+
+  const token = authorization.replace(/^Bearer\s/i, '');
   let payload;
+
   try {
-    payload = jwt.verify(
-      authorization.replace('Bearer ', ''),
-      '5066e19ca0f3f2521d7c7ef07d99c7307c5533cea846d26f4b9408932f1fc99f',
-    );
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new UnauthorizedError('Сессия истекла. Повторите авторизацию.'));
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
+
   req.user = payload;
-  next();
+
+  return next();
 };
