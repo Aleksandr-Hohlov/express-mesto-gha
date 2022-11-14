6c502dcb1d1929d4fc3a87c3bf7d5affd96a8317
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-15 01:04:47 +0300

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,7 +41,7 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
-/*
+
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
@@ -57,5 +58,5 @@ userSchema.statics.findUserByCredentials = function (email, password) {
       });
     });
 };
-*/
+
 module.exports = mongoose.model('user', userSchema);
