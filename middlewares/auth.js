574ab13f6cf3907bcc8badfe4dd2a3e70b7f16a1
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-15 01:01:45 +0300

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,10 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(authorization.replace('Bearer ', ''), 'some-secret-key');
+    payload = jwt.verify(
+      authorization.replace('Bearer ', ''),
+      '5066e19ca0f3f2521d7c7ef07d99c7307c5533cea846d26f4b9408932f1fc99f',
+    );
   } catch (err) {
     next(new UnauthorizedError('Сессия истекла. Повторите авторизацию.'));
   }
