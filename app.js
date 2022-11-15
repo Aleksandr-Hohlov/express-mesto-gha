4f8dcb9fd03541601af5af6c43cbc9d55cc5397f
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-16 00:59:38 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 
+const app = express();
+const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const NotFoundError = require('./errors/NotFoundError');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
@@ -12,16 +20,6 @@ const { messageErr } = require('./constants/constants');
 const handleErrors = require('./middlewares/handleErrors');
 const auth = require('./middlewares/auth');
 const { createUserValidation, loginValidation } = require('./middlewares/validation');
-
-const { PORT = 3000 } = process.env;
-
-const app = express();
-
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.post('/signin', loginValidation, loginUser);
 app.post('/signup', createUserValidation, createUser);
