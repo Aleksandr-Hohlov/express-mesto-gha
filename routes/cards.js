51d6ed1a308126d1a306fa3acb2573e181a8458f
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-16 00:27:25 +0300

    Add files via upload

--- routes/cards.js
+++ routes/cards.js
@@ -1,17 +1,59 @@
 const router = require('express').Router();
-
+const { celebrate, Joi } = require('celebrate');
+// prettier-ignore
 const {
-  getAllCards,
-  createCard,
-  deleteCard,
-  likeCard,
-  dislikeCard,
+  getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/', getAllCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+/* роутеры :
+GET /cards — возвращает все карточки
+POST /cards — создаёт карточку
+DELETE /cards/:cardId — удаляет карточку по идентификатору
+PUT /cards/:cardId/likes — поставить лайк карточке
+DELETE /cards/:cardId/likes — убрать лайк с карточки  */
+router.get('/', getCards);
+
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string()
+        .required()
+        .pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+    }),
+  }),
+  createCard,
+);
+
+router.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  deleteCard,
+);
+
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  likeCard,
+);
+
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  dislikeCard,
+);
 
 module.exports = router;
