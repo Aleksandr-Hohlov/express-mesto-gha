5ac00cc413a530dfdea1623e77b96b97b6eb1169
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-15 23:29:24 +0300

    Add files via upload

--- controllers/users.js
+++ controllers/users.js
@@ -94,6 +94,12 @@ const loginUser = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
+      });
+
       res.send({ token });
     })
     .catch(() => {
